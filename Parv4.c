9a014812d3375278606487279c8e09e0a9af2440
ParvDoshi <parvdoshi6117@gmail.com>
2025-08-08 22:16:30 +0530

    Add files via upload

---
+++ Parv4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void){
+    int num1,num2,divide;
+
+    printf("First number :");
+    scanf("%d",&num1);
+
+    printf("Second number :");
+    scanf("%d",&num2);
+
+    divide= num1/num2;
+
+    printf("This is my divide :%d", divide);
+    return 0;
+}
+
+
