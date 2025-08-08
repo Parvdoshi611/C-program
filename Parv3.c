05f66e37e87347daaaf5511287357edcfb815d34
ParvDoshi <parvdoshi6117@gmail.com>
2025-08-08 22:10:32 +0530

    Add files via upload

---
+++ Parv3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+    int num1,num2,multiply;
+
+    printf("First number :");
+    scanf("%d",&num1);
+
+    printf("Second number :");
+    scanf("%d",&num2);
+
+    multiply= num1*num2;
+
+    printf("This is my multply :%d", multiply);
+    return 0;
+}
