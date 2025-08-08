85ed764121f2d34a2cd114c22da246d55520f2f1
ParvDoshi <parvdoshi6117@gmail.com>
2025-08-08 10:37:57 +0530

    Add files via upload

---
+++ Parv1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+    int num1,num2,sum;
+
+    printf("First number :");
+    scanf("%d",&num1);
+
+    printf("Second number :");
+    scanf("%d",&num2);
+
+    sum= num1+num2;
+
+    printf("This is my sum :%d", sum);
+    return 0;
+}
