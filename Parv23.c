3db8e52f3b43f512cdf64d0c19216f651a08bdf7
ParvDoshi <parvdoshi6117@gmail.com>
2025-08-21 18:44:17 +0530

    Parv23.c

--- Parv23.c
+++ Parv23.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 
 int main(void) {
-    float sub1, sub2, sub3, total, average;
+    int sub1, sub2, sub3, total, average;
 
     
-    printf("Enter marks of subject 1: ");
-    scanf("%f", &sub1);
+    printf("Enter marks of sub1: ");
+    scanf("%d", &sub1);
 
-    printf("Enter marks of subject 2: ");
-    scanf("%f", &sub2);
+    printf("Enter marks of sub2: ");
+    scanf("%d", &sub2);
 
-    printf("Enter marks of subject 3: ");
-    scanf("%f", &sub3);
+    printf("Enter marks of sub3: ");
+    scanf("%d", &sub3);
     
     total = sub1 + sub2 + sub3;
 
     average = total / 3;
 
-    printf("Total marks is %f\n", total);
-    printf("Average marks is %f\n", average);
+    printf("Total marks is %d\n", total);
+    printf("Average marks is %d\n", average);
 
     return 0;
 }
