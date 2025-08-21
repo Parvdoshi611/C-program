f38db531c048c14fb852a5a0610e0d87cb460317
ParvDoshi <parvdoshi6117@gmail.com>
2025-08-21 18:31:47 +0530

    Parv19.c

--- Parv19.c
+++ Parv19.c
@@ -4,14 +4,14 @@ int main(void) {
     float r, a;
 
     
-    printf("Enter the radius ");
+    printf("Enter the vakue of r ");
     scanf("%f", &r);
 
     
     a = (22.0 / 7.0) * r * r;
 
     
-    printf("Area of circle is %f\n", a);
+    printf("Area is %f\n", a);
 
     return 0;
 }
