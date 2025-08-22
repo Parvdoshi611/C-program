7a1e99de3eb4ea6de44b7da1a2fb23c4a0ecb181
ParvDoshi <parvdoshi6117@gmail.com>
2025-08-22 10:08:04 +0530

    Add files via upload

---
+++ ifelse Parv2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(){
+  int a,b,c;
+  printf("Value of three number :");
+  scanf("%d %d %d",&a,&b,&c);
+
+  if(a>b && a>c){
+    printf("%d is greatest",a);
+  }
+  else if(b>a && b>c){
+    printf("%d is greatest",b);
+  }
+  else if (c>a && c>b){
+    printf("%d is greatest",c);
+  }
+   if(a<b && a<c){
+    printf("%d is smallest",a);
+  }
+  else if(b<a && b<c){
+    printf("%d is smallest",b);
+  }
+  else if (c<a && c<b){
+    printf("%d is smallest",c);
+  }
+  returm 0;
+}
