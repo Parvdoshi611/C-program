3fbaecdc223f36e8e76773b01eb4a8a15e081d5d
ParvDoshi <parvdoshi6117@gmail.com>
2025-08-22 10:32:51 +0530

    Add files via upload

---
+++ Ifelse Parv4.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+     int num;
+
+     printf("Enter any number:");
+     scanf("%d",&num);
+
+     if(num%7==0){
+        printf("%d is divisible by 7",num);
+     }
+     else
+        printf("%d is not divisible by 7",num);
+     return 0;
+
+}
